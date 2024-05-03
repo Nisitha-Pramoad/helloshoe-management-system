fd3df112b17e69691e9dc3032a1ec40988c2dec6
Nisitha-Pramoad <nisithapramoad99@gmail.com>
2024-05-03 15:07:34 +0530

    implement customer, item, order functions

---
+++ src/main/java/lk/helloshoe/shoeshopmanagement/ShoeshopmanagementApplication.java
@@ -0,0 +1,13 @@
+package lk.helloshoe.shoeshopmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeshopmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeshopmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/helloshoe/shoeshopmanagement/ShoeshopmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.helloshoe.shoeshopmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeshopmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
